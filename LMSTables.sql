6389c26a2db54f81ccea6b7c6c7b8bfc14b88b1a
LeaveManagementSys <55281806+LeaveManagementSys@users.noreply.github.com>
2019-09-30 16:55:27 +0530

    Update LMSTables.sql

--- LMSTables.sql
+++ LMSTables.sql
@@ -9,9 +9,9 @@ select * from Leave;
 
 create table Employee(employeeId varchar(10) primary key,firstname varchar(15),lastname varchar(15),email varchar(20),dob date,mobileno number(10),hiredate date,salary number(10),managerId varchar(10),hrid varchar(10),address varchar(20),password varchar(10),designation varchar(20));
 
-insert into Employee values('E100','vamshi','vemula','vamshi@gmail.com','15-JAN-1997',9098989879,'19-AUG-19',27000,'M100','H100','malkajgiri','vamshi123','associate engineer');
-insert into Employee values('E101','swapna','N','swapna@gmail.com','10-JAN-1996',9098988779,'19-AUG-19',27600,'M101','H101','hi-tech city','swapna123','associate engineer');
-insert into Employee values('E102','gayatrhi','p','gayathri@gmail.com','01-JAN-1997',7098989879,'19-AUG-19',27900,'M100','H100','gachibowli','gayathri12','software engineer');
+insert into Employee values('E100','vamshi','vemula','vamshi@gmail.com','15-JAN-1997',9098989879,'19-AUG-19',27000,'M100','malkajgiri','vamshi123','associate engineer');
+insert into Employee values('E101','swapna','N','swapna@gmail.com','10-JAN-1996',9098988779,'19-AUG-19',27600,'M101','hi-tech city','swapna123','associate engineer');
+insert into Employee values('E102','gayatrhi','p','gayathri@gmail.com','01-JAN-1997',7098989879,'19-AUG-19',27900,'M100','gachibowli','gayathri12','software engineer');
 
 create table HR(hr_id varchar(10),firstname varchar(20),lastname varchar(20),designation varchar(10),email varchar(20),password varchar(10));
 
